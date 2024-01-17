b4249b0c6420cebc31d442da48e9ad8d0c0c9c47
meo2326cc <cccmsp1@gmail.com>
2024-01-17 22:39:24 +0800

    add Hero text effect & document.title

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -39,13 +39,22 @@ export default function Hero() {
 
   return (
     <div className=' bg-cover min-h-screen relative' ref={heroEl}>
-      <div className='border absolute left-5 right-5 top-20 bottom-20 md:left-20 md:right-20 m-auto flex justify-around items-center rounded-xl bg-no-repeat bg-cover p-10 flex-wrap md:flex-nowrap ' style={{ backgroundImage: `url(${city})` }} >
-        <div className='relative' ref={(el) => { moveEls.current.push(el) }}>
+      <div className='overflow-hidden border absolute left-5 right-5 top-20 bottom-20 md:left-20 md:right-20 m-auto flex justify-around items-center rounded-xl bg-no-repeat bg-cover p-10 flex-wrap md:flex-nowrap ' style={{ backgroundImage: `url(${city})` }} >
+      <div className="w-[1400px] absolute text-stroke opacity-70 text-transparent -left-10 mix-blend-overlay italic noto-sans-black" style={{fontSize:'160px'}}>
+        <p className="leading-none">臭氧</p>
+        <p className="leading-none">pm10懸浮微粒</p>
+        <p className="leading-none">二氧化氮</p>
+        <p className="leading-none">pm2.5細懸浮微粒</p>
+        <p className="leading-none">二氧化硫</p>
+        <p className="leading-none">氮氧化物</p>
+        <p className="leading-none">空氣污染指標物</p>
+        </div>
+        <div className='relative drop-shadow-xl' ref={(el) => { moveEls.current.push(el) }}>
           <Decorations />
           <h1 className='text-center md:text-left drop-shadow-lg text-3xl md:text-5xl md:leading-tight lg:leading-tight bg-clip-text tracking-wider noto-sans-black bg-gradient-to-r from-blue-800 to-cyan-600 text-transparent'> <span className='block w-full md:w-32'>方便快速</span>空氣監測小幫手</h1>
           <h2 className='text-center md:text-left text-md md:text-xl lg:text-2xl mt-2 noto-sans-bold'>line 空氣品質監測機器人</h2>
         </div>
-        <div className=" relative" ref={(el) => { moveEls.current.push(el) }}>
+        <div className=" relative drop-shadow-xl" ref={(el) => { moveEls.current.push(el) }}>
           <div className='bg-white hidden sm:block border border-green-600 rounded-lg text-center overflow-hidden mb-3'>
             <img className='p-2 w-44' src={qr} alt="linebot空氣品質監測掃描加入好友" />
  
